e2d2f14e209edb73f779aa41c8e3bbbb63c0f167
Mourat Achmet oglou Iliaz <40266759+machmetoglouiliaz@users.noreply.github.com>
2024-09-27 07:23:27 +0300

    26-09 training files (Beginning)

---
+++ Training-26-09/Cpp-project-26-09/Cpp-project-26-09.cpp
@@ -0,0 +1,19 @@
+/*****************************************
+* Copyright
+* Name:    Mourat Achmet oglou Iliaz
+* VAT:     156005141
+* Date:    26.09.2024
+* All rights reserved.
+*****************************************/
+
+#include <iostream>
+
+/**
+ * Prints "Hello World"
+ * @return Exit status
+*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
